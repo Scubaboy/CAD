ce59fa2e4b0b92423312a167f6113b831484df19
Rob Cawsey <rob.cawsey@cirruslogistics.com>
2023-10-31 23:14:59 +0000

    Initial commit

---
+++ CAD/Components/Behaviours/MouseObjectSelectionBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using Evergine.Common.Input.Mouse;
+using Evergine.Framework;
+using Evergine.Framework.Graphics;
+using System;
+
+namespace CAD.Components.Behaviours
+{
+    public class MouseObjectSelectionBehaviour : Behavior
+    {
+        [BindComponent(source: BindComponentSource.Scene, isRequired: true)]
+        private Camera3D _camera = null;
+
+        private MouseDispatcher _mouseDispatcher;
+
+        protected override void OnActivated()
+        {
+            _mouseDispatcher = this._camera.Display?.MouseDispatcher;
+        }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
